807f8319d29db295eec4efdad8a0d9e41036018c
Humakt83 <urlact@yahoo.com>
2015-12-16 13:00:10 +0200

    Change to module project and add GWT WIP

---
+++ voteria-client/src/main/java/fi/ukkosnetti/voteria/client/Voteria.java
@@ -0,0 +1,13 @@
+package fi.ukkosnetti.voteria.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Voteria implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
